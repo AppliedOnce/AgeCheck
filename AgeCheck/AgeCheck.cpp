565d925bf4cf6ff2d1cc36d658eab079bfa7c9a6
AppliedOnce <mattistabell@gmail.com>
2020-09-01 14:07:00 +0200

    Finished task

--- AgeCheck/AgeCheck.cpp
+++ AgeCheck/AgeCheck.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+int age = 0;
+
 int main()
 {
-    
+    std::cout << "Tell me your age: ";
+    std::cin >> age;
+    if (age >= 20)
+    {
+        std::cout << "You are an adult.";
+    }
+    else
+    {
+        std::cout << "You are a teenager.";
+    }
 }
